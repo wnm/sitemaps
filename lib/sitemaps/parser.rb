5987eb416d6ac82443c4e1845f3e11a19f8425b4
Jon Raphaelson <jraphaelson@termscout.com>
2016-03-21 11:02:31 -0600

    fixes to support ruby < 2.3

--- lib/sitemaps/parser.rb
+++ lib/sitemaps/parser.rb
@@ -1,9 +1,9 @@
 module Sitemaps
   module Parser
-    require "rexml/document"
     require "time"
-
-    VALID_CHANGEFREQ = %w(always hourly daily weekly monthly yearly never)
+    require "rexml/document"
+    require "active_support"
+    require "active_support/core_ext/object/try"
 
     def self.parse(source)
       document = REXML::Document.new(source)
@@ -29,23 +29,24 @@ module Sitemaps
     end
 
     def self.parse_loc(root)
-      loc  = root.get_text("loc")&.value()
+      loc  = root.get_text("loc").try(:value)
       loc && URI.parse(loc) rescue nil
     end
 
     def self.parse_lastmod(root)
-      mod  = root.get_text("lastmod")&.value()
+      mod  = root.get_text("lastmod").try(:value)
       mod && Time.parse(mod) rescue nil
     end
 
+    VALID_CHANGEFREQ = %w(always hourly daily weekly monthly yearly never)
     def self.parse_changefreq(root)
-      freq  = root.get_text("changefreq")&.value()
+      freq  = root.get_text("changefreq").try(:value)
       freq && VALID_CHANGEFREQ.include?(freq) ? freq.to_sym : nil
     end
 
     def self.parse_priority(root)
-      priority  = root.get_text("priority")&.value() || "0.5"
-      priority && Float(priority) rescue 0.5
+      priority  = root.get_text("priority").try(:value) || "0.5"
+      priority && Float(priority) rescue 0.5 # default priority according to spec
     end
   end
 end
--- sitemaps.gemspec
+++ sitemaps.gemspec
@@ -19,8 +19,10 @@ Gem::Specification.new do |spec|
   spec.files         = files
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.11"
+  spec.add_development_dependency "bundler", "~> 1"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "byebug", "~> 8.2"
+
+  spec.add_runtime_dependency "activesupport", "~> 4"
 end
